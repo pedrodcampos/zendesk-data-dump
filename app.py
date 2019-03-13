f93399f22834e14786c2b2d06c76d8ff8f7e269f
Pedro Campos <pedro.campos@trilogy.com>
2019-03-13 11:18:55 -0300

    retrives articles from hc, inject data and export to csv

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from settings import ZENDESK_HC_API_URL, AUTH
+from helpers import inject, to_csv
 from zendesk import ZendeskHC
+
 
 zendesk_hc = ZendeskHC(ZENDESK_HC_API_URL, AUTH)
 
-articles, users, categories, sections = zendesk_hc.articles(
-    include='users,categories,sections')
+
+if __name__ == '__main__':
+    articles, users, categories, sections = zendesk_hc.articles(
+        include='users,categories,sections')
+
+    articles = inject(users, 'author_id', articles, 'author')
+    articles = inject(sections, 'section_id', articles, 'section')
+    for article in articles:
+        article['section'] = inject(
+            categories, 'category_id', article['section'], 'category')
+
+    data = [{'id': article['id'],
+             'author_name': article['author']['name'],
+             'title': article['title'],
+             'created_at': article['created_at'],
+             'edited_at': article['edited_at'],
+             'updated_at': article['updated_at'],
+             'category_name': article['section']['category']['name'],
+             'section_name':article['section']['name']} for article in articles
+
+            ]
+    to_csv(data)

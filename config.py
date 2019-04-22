2179c2e073ea1ab2f7b088f88925a44e05a3086e
Pedro Campos <pedrocamp@gmail.com>
2019-04-22 16:31:08 -0300

    parsing .env file as dict

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-from os import environ
+from os import path
+
+
+def load_env():
+    env = {}
+    filename = path.join(path.curdir, '.env')
+    with open(filename) as envfile:
+        for line in envfile:
+            key, value = line.split("=")
+            env.update({key: value})
+    return env
 
 
 def build_zendesk_config(instance=None, helpcenter=None, username=None, password=None):
-
+    environ = load_env()
     return {
         'instance': instance if instance else environ.get('INSTANCE', None),
         'helpcenter': helpcenter if helpcenter else environ.get('HELPCENTER', None),
